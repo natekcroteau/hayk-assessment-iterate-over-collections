1701b16338a56280eb1bca5265f2f0ac2e8eb3b8
Nate Croteau <natekcroteau@gmail.com>
2020-12-14 10:30:57 -0700

    commit tree 95341a508eb6fc5e181420684b4bccba0ac83d94

--- app.rb
+++ app.rb
@@ -14,3 +14,20 @@ companies = [
 ]
 
 ### Your code below this
+def list_of_strings_name_and_size
+    companies.map do |name, size|
+    binding.pry 
+    end
+end
+
+
+
+
+
+
+# * Generate a list of strings with the name and size (eg. "Alpha - 30")
+# * Combine all the sizes with reduce
+# * Filter the list to show only companies over 100
+# * Find the company named "Beta"
+# * Find the largest company
+# * Sort the companies from largest to smallest
